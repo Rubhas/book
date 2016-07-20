cd93cb70db9314be55ec0dfcb19c4dd7647381f1
Rubhas <rubhabe@gmail.com>
2016-07-20 15:25:20 +0530

    Update and rename Ss.java to Sss.java

---
+++ Sss.java
@@ -0,0 +1,11 @@
+ublic class Sss {
+
+	public static void main(String[] args){
+	char c=args[0].charAt(0);
+		if(c>='a' && c<='z'||c>='A' && c<='Z'){
+System.out.println(c+" is alphabet");
+	}
+		else{
+			System.out.println(c+" is not an alphabet");
+		}
+	}}
