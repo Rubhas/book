0f8c326307af09438fec7cb442c10b312ccdab8c
Rubhas <rubhabe@gmail.com>
2016-07-20 15:33:28 +0530

    Create eve.java

---
+++ eve.java
@@ -0,0 +1,13 @@
+public class eve {
+ 
+ 	public static void main(String[] args) {
+ 	int num=Integer.parseInt(args[0]);
+ 	if(num%2==0){
+ 		System.out.println("num is odd");
+ 	}
+ 	else{
+ 		System.out.println("num is even");
+ 	}
+ 	}
+ 
+ }
