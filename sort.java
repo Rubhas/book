732b9555d8b2da559b50627a81910be809c8be65
Rubhas <rubhabe@gmail.com>
2016-07-21 15:34:33 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,14 @@
+public class sort {
+
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		if(n%2==0){
+			System.out.println("it is even");
+		}
+		else{
+			System.out.println("it is odd");
+		}
+
+	}
+
+}
